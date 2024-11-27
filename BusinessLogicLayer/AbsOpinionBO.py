a11b8b428ab0767ef43225905aa7cfbedd9275fc
Syed Ali Masood Bukhari <157227972+SyedAliMasoodBukhari@users.noreply.github.com>
2024-11-27 22:21:35 +0500

    Delete BusinessLogicLayer directory from Main

--- BusinessLogicLayer/AbsOpinionBO.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC,abstractmethod
-from TO.OpinionTO import OpinionTO
-from typing import List
-from TO.NarratorTO import NarratorTO
-class AbsOpinionBO(ABC):
-    @abstractmethod
-    def getOpinions(self,narratorTO:NarratorTO)->List[OpinionTO]:
-        pass
-    
