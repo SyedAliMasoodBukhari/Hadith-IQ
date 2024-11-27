82763436336a1a9b60dd4805ca804234079ecbea
Syed Ali Masood Bukhari <157227972+SyedAliMasoodBukhari@users.noreply.github.com>
2024-11-27 22:23:37 +0500

    Delete BusinessLogicLayer directory from Ali Masood branch

--- BusinessLogicLayer/OpinionBO.py
+++
@@ -1,9 +0,0 @@
-from AbsOpinionBO import AbsOpinionBO
-from TO.OpinionTO import OpinionTO
-from typing import List
-from TO.NarratorTO import NarratorTO
-class OpinionBO(AbsOpinionBO):
-    
-    def getOpinions(self,narratorTO:NarratorTO)->List[OpinionTO]:
-        return None
-    
